249974c02d4ebc65d9fa3da3cc2ee378b21217c6
Daniel Wilczewski <daniel.wilczewski@sonalake.com>
2019-10-19 23:20:18 +0200

    bsm 1

--- App.js
+++ App.js
@@ -1,114 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {
+  YellowBox,
   SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
+  Button,
   StatusBar,
+  AsyncStorage,
+  Alert,
 } from 'react-native';
+import {TextInput} from 'react-native-paper';
+YellowBox.ignoreWarnings(['AsyncStorage']);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const storeData = async (key, data) => {
+  try {
+    return await AsyncStorage.setItem(key, data);
+  } catch (error) {
+    Alert.alert("Error - can't save to storage", error);
+  }
+};
 
-const App: () => React$Node = () => {
+const retrieveData = async key => {
+  try {
+    return await AsyncStorage.getItem(key);
+  } catch (error) {
+    Alert.alert("Error - can't retreive from storage", error);
+  }
+};
+
+const App = () => {
+  const pwdRef = useRef();
+  const [passwordFromStorage, setPasswordFromStorage] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+  const [noteText, setNoteText] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    retrieveData('@BSM_PASS').then(pass => {
+      if (pass) {
+        setPasswordFromStorage(pass);
+      } else {
+        setPasswordFromStorage('startowe');
+      }
+    });
+
+    retrieveData('@BSM_NOTE').then(note => {
+      if (note) {
+        setNoteText(note);
+      }
+    });
+  }, []);
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
+      <SafeAreaView
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          paddingHorizontal: 25,
+        }}>
+        {isLoggedIn && (
+          <TextInput
+            label="Note"
+            value={noteText}
+            multiline
+            onChangeText={text => {
+              setNoteText(text);
+              storeData('@BSM_NOTE', text);
+            }}
+            style={{marginBottom: 30}}
+            numberOfLines={10}
+          />
+        )}
+
+        <TextInput
+          ref={pwdRef}
+          mode="outlined"
+          label={isLoggedIn ? 'Change password' : 'Enter password'}
+          value={passwordInput}
+          onChangeText={setPasswordInput}
+          style={{marginBottom: 10}}
+          secureTextEntry
+        />
+        <Button
+          title={isLoggedIn ? 'Change password' : 'Log in'}
+          color="black"
+          onPress={() => {
+            if (!isLoggedIn && passwordInput !== passwordFromStorage) {
+              Alert.alert('Incorrect password');
+            } else if (!isLoggedIn && passwordInput === passwordFromStorage) {
+              setIsLoggedIn(true);
+              Alert.alert('Hello!');
+              pwdRef.current.blur();
+            } else if (isLoggedIn && passwordInput.length) {
+              storeData('@BSM_PASS', passwordInput).then(() => {
+                Alert.alert('Password has been changed');
+                pwdRef.current.blur();
+              });
+            }
+          }}
+        />
       </SafeAreaView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
